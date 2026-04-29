6fc5b4c64442574192b6a6d25b2984836a9d5db9
grq <gaorongqiang@dadi-es.onaliyun.com>
2026-04-29 17:49:01 +0800

    123

--- api/message.js
+++ api/message.js
@@ -8,9 +8,9 @@
         "title": "上新通知",
         "content": "App新增xxx等功能",
         "time": "2026-09-23",
-        "type": "sys/warning",
-        "isTips": false,
-        "tipsMsg": ""
+        "type": "warning",
+        "isTips": true,
+        "tipsMsg": "sss"
       }
     ]
   }
